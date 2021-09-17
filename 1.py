a7d0a4dcfe3fd910adff559fbeccb0db3f910034
Daria706 <Ilicheva-DM2020@sgugit.ru>
2021-09-17 18:37:00 +0700

    Task 3.1

--- 1.py
+++ 1.py
@@ -7,3 +7,4 @@ def num(n):
         s += k * a
     return s
 print("Сумма = ", num(n))
+
--- Task 3.py
+++ Task 3.py
@@ -1,8 +1,1 @@
-s = 0
-a = 1
-n: list[int] =[1, 3, 5, 6, 11, 9, 1, 12]
-for number in range(n):
-    a *= k % 2
-    s += k * a
-print(s)
 
